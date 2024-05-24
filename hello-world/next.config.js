774dbff283e18693610e2f950900f9be4c86011c
chapman91 <chapmandoneil91@gmail.com>
2024-05-24 13:19:46 -0400

    Nested Routing #6

---
+++ hello-world/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
+// 'reactStrictMode' Development mode is a feature for highlighting problems in an application
+// Identify and save lifecycles, legacy API usage, and a number of other features 
---
+++ hello-world/src/pages/_app.js
@@ -0,0 +1,7 @@
+import '@/styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+// This file is used define the layout for your application
